91edf20a17f2c4438709ad6ab927418f6f8dfd7f
am401 <54643953+am401@users.noreply.github.com>
2021-10-10 01:11:32 -0500

    Update to sanitize data

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,12 @@
 # Changelog
-## [ 0.1 ] - 01-14-2021 
+## [ 0.0.2 ] - 10-10-2021
+### Added
+- Function to sanitize username and password. This is still a work in progress where multiple username and passwords are provided within a single request
+### Updated
+- Changed how the log queries are put together to factor in the sanitized information
+### Removed
+- For the time being removed the WordPress admin page hooks
+
+## [ 0.0.1 ] - 09-14-2021 
 ###  Added
 - Initial commit
--- xmlrpc-logger.php
+++ xmlrpc-logger.php
@@ -9,8 +9,8 @@
     License: GPL 1.2
 */
 
-function xl_get_user_ip() {
-    // Gather IP address from requester
+// Gather IP address from requester
+function amxml_get_user_ip() {
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
         $ip = $_SERVER['HTTP_CLIENT_IP'];
     } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
@@ -21,17 +21,31 @@ function xl_get_user_ip() {
     return $ip;
 }
 
-function xl_log_xmlrpc_request() {
+function amxml_sanitize($xml_body) {
+    $search = "/(.*<data><value><string>)(.*)(<\/string><\/value><value><string>)(.*)(<\/string><\/value>.*)/s";
+    $replace_user = "SANITIZED_USERNAME";
+    $replace_pass = "SANITIZED_PASSWORD";
+    $string = $xml_body;
+
+    return preg_replace($search, '$1'.$replace_user.'$3'.$replace_pass.'$5', $string);
+}
+    
+
+// Grab the xmlrpc.php request body
+function amxml_log_xmlrpc_request() {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         if ( defined( 'XMLRPC_REQUEST' ) ) {
             // Retrieve the IP address returned from the function
-            $ip = xl_get_user_ip();
+            $ip = amxml_get_user_ip();
+            $raw_xml = preg_replace('/[\r\n]+/', "", file_get_contents("php://input"));
+            $sanitized_xml = amxml_sanitize($raw_xml);
 
             // Gather the raw data from the request
             $logentry = "[" . date('D M H:i:s Y') . "] [XMLRPC Request Data] ";
             $logentry .= "[" . $ip . "] ";
-            $logentry .= preg_replace('/[\r\n]+/', "", file_get_contents("php://input"));
+            $logentry .=  $sanitized_xml;
+            /*$logentry .= $raw_xml;*/
 
             // File location
             $logfile = ABSPATH.'/wp-content/xmlrpc-request.log';
@@ -42,5 +56,38 @@ function xl_log_xmlrpc_request() {
     }
 }
 
-add_action('init', 'xl_log_xmlrpc_request');
+/*
+add_action('admin_menu', 'amxml_register_sub_menu');
+
+function amxml_register_sub_menu() {
+    add_submenu_page(
+        'tools.php',
+        'XMLRPC-Logger',
+        'XMLRPC-Logger Settings',
+        'manage_options',
+        'xmlrpc-logger-settings',
+        'axml_display_xmlrpc_logger'
+    );
+}
+
+function axml_display_xmlrpc_logger() {
+    echo '<div class="wrap">';
+        echo '<h2>Page Title</h2>';
+    echo '</div>';
+}
+
+add_action( 'admin_menu', 'amxml_settings_page');
+
+function amxml_settings_page() {
+    add_options_page(
+        'XMLRPC Logger',
+        'XMLRPC Logger Settings',
+        'manage_options',
+        'xmlrpc-logger-settings',
+        'amxml_xmlrpc_logger_options'
+    );
+}
+ */
+
+add_action('init', 'amxml_log_xmlrpc_request');
 ?>
