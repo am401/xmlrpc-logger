9bab61ebcf25873872b4f7c91deab8e65e0ec343
am401 <54643953+am401@users.noreply.github.com>
2021-11-17 20:18:08 -0600

    Added readme.txt and removed temp code from main file

---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Plugin Name ===
+Contributors: am401
+Donate link: na
+Tags: xmlrpc
+Requires at least: 4.7
+Tested up to: 5.8
+Stable tag: 4.3
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+A simple logger to capture incoming data to the xmlrpc.php file.
+
+=== Description ===
+
+Ever wondering what's happening within your xmlrpc.php file? The XML-RPC interface within WordPress allows access to the WP Admin area to carry out tasks such as publishing posts or uploading media. However, there are a number of potential security implications that may affect your site. The primary attack vector that can be used against the XML-RPC interface is username and password brute forcing.
+
+Being able to see what's happening with requests to this interface, especially when you are not actively using it yourself can bring additional security insight when you are trying to protect your site from attackers.
+
+=== Changelog ===
+
+= 0.0.2 =
+* Removed WordPress admin page hooks.
+* Changed how the log queries and formatting are put together.
+
+= 0.0.1 =
+* Initial commit.
--- xmlrpc-logger.php
+++ xmlrpc-logger.php
@@ -21,40 +21,18 @@ function amxml_get_user_ip() {
     return $ip;
 }
 
-/*
-function amxml_sanitize($xml_body) {
-    $search = "/(.*<data><value><string>)(.*)(<\/string><\/value><value><string>)(.*)(<\/string><\/value>.*)/";
-    $replace_user = "SANITIZED_USERNAME";
-    $replace_pass = "SANITIZED_PASSWORD";
-    $string = $xml_body;
-
-    return preg_replace($search, '$1'.$replace_user.'$3'.$replace_pass.'$5', $string);
-}
- */
-
 // Grab the xmlrpc.php request body
-function amxml_log_xmlrpc_request() {
+function xl_log_xmlrpc_request() {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         if ( defined( 'XMLRPC_REQUEST' ) ) {
             // Retrieve the IP address returned from the function
-            $ip = amxml_get_user_ip();
-            /*
-            $input_to_sanitize = preg_replace('/[\r\n]+/', "", file_get_contents("php://input"));
-            $pattern_sanitize = '/(.*<data><value><string>)(.*)(<\/string><\/value><value><string>)(.*)(<\/string><\/value>.*)/';
-            $sanitized_xml = preg_replace_callback($pattern_sanitize, 'amxml_sanitize_data', $input_to_sanitize);
- */
-            //$xml_raw = file_get_contents("php://input");
-            $xml_string = new SimpleXMLElement(file_get_contents("php://input"));
-            $xml_data = $xml_string->asXML();
-            $xml_output = $xml_data->params->param->value->array->data->value->struct->member->value->array->data->value->array->data->string;
+            $ip = xl_get_user_ip();
 
             // Gather the raw data from the request
             $logentry = "[" . date('D M H:i:s Y') . "] [XMLRPC Request Data] ";
             $logentry .= "[" . $ip . "] ";
-            $logentry .= $xml_output;
-            //$logentry .= $xml_raw;
-            /*$logentry .= $raw_xml;*/
+            $logentry .= preg_replace('/[\r\n]+/', "", file_get_contents("php://input"));
 
             // File location
             $logfile = ABSPATH.'/wp-content/xmlrpc-request.log';
@@ -64,39 +42,5 @@ function amxml_log_xmlrpc_request() {
         }
     }
 }
-
-/*
-add_action('admin_menu', 'amxml_register_sub_menu');
-
-function amxml_register_sub_menu() {
-    add_submenu_page(
-        'tools.php',
-        'XMLRPC-Logger',
-        'XMLRPC-Logger Settings',
-        'manage_options',
-        'xmlrpc-logger-settings',
-        'axml_display_xmlrpc_logger'
-    );
-}
-
-function axml_display_xmlrpc_logger() {
-    echo '<div class="wrap">';
-        echo '<h2>Page Title</h2>';
-    echo '</div>';
-}
-
-add_action( 'admin_menu', 'amxml_settings_page');
-
-function amxml_settings_page() {
-    add_options_page(
-        'XMLRPC Logger',
-        'XMLRPC Logger Settings',
-        'manage_options',
-        'xmlrpc-logger-settings',
-        'amxml_xmlrpc_logger_options'
-    );
-}
- */
-
 add_action('init', 'amxml_log_xmlrpc_request');
 ?>
